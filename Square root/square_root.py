0e805aa3f1f620e48f212e9a6d903420630ca362
CJeswinSunny <cjeswinsunny@gmail.com>
2026-02-24 22:18:48 +0530

    Update square_root.py

--- Square root/square_root.py
+++ Square root/square_root.py
@@ -7,13 +7,10 @@ def square_root(n, tolerance=1e-10):
     if n == 0:
         return 0.0
 
-    # Initial guess
     guess = n / 2.0 if n > 1 else 1.0
 
     while True:
         next_guess = 0.5 * (guess + n / guess)
-        # If the difference between the next guess and the current guess is less than the tolerance,
-        # we have found the square root to our desired precision.
         if abs(guess - next_guess) < tolerance:
             return next_guess
         guess = next_guess
